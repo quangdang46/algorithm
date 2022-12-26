79587961f2507dbbcb2cc4196242f11cf42a8775
Trần Quang Đãng <91641923+quangdang46@users.noreply.github.com>
2022-12-26 09:43:02 +0700

    Create back.js

---
+++ back.js
@@ -0,0 +1,55 @@
+function solveEightQueens(board, row) {
+  if (row === 8) {
+    return true;
+  }
+
+  for (let col = 0; col < 8; col++) {
+    if (isSafe(board, row, col)) {
+      board[row][col] = 1;
+
+      if (solveEightQueens(board, row + 1)) {
+        return true;
+      }
+
+      board[row][col] = 0;
+    }
+  }
+
+  return false;
+}
+
+function isSafe(board, row, col) {
+  for (let i = 0; i < 8; i++) {
+    if (board[i][col] === 1) {
+      return false;
+    }
+  }
+
+  for (let i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+    if (board[i][j] === 1) {
+      return false;
+    }
+  }
+  for (let i = row, j = col; i >= 0 && j < 8; i--, j++) {
+    if (board[i][j] === 1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Initialize the board to an empty 8x8 grid
+const board = [[0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0]];
+
+if (solveEightQueens(board, 0)) {
+  console.log("Solution found:", board);
+} else {
+  console.log("No solution found.");
+}
